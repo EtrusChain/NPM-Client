2cfb329e650210abadb4242637e290e1a908e4f2
yurikaza <yusufmirza55@hotmail.com>
2023-01-14 21:27:43 +0300

    version update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,5 @@
-/// <reference types="node" />
 import { IsendFile } from './@interface/sendFiles';
 import { IgetFiles } from './@interface/getFiles';
 export declare const Greeter: (name: string) => string;
-export declare const AddFile: (fileBuffer: Buffer[], fileName: string) => Promise<IsendFile>;
+export declare const AddFile: (fileBuffer: any[], fileName: string) => Promise<IsendFile>;
 export declare const GetFile: (fileHash: string, fileName: string) => Promise<IgetFiles>;
